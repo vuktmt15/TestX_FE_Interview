958a6ba46067ad947ab129841aafc38a75a6d52b
Nguyễn Văn Vũ <85126015+vuktmt15@users.noreply.github.com>
2023-12-14 16:50:34 +0700

    add secret next-auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,16 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+const options = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-});
+  secret: process.env.GOOGLE_SECRET,
+};
+
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
